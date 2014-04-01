99c456351b0c9b6d0a772368688bf35c6a65888e
Naveen Malik <nmalik@redhat.com>
2014-03-31 21:11:17 -0400

    WIP to get events to show routes when you click a house in a 'real' page

---
+++ src/main/webapp/data/poi.js
@@ -0,0 +1,385 @@
+var poiData = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "_id": "default|35.740399|-78.859281",
+            "type": "Feature",
+            "geometry": {
+                "coordinates": [
+                    -78.859280999999996,
+                    35.740398900000002
+                ],
+                "type": "Point"
+            },
+            "properties": {
+                "address": "700 Brittley Way, Apex, NC 27502, USA",
+                "categories": [
+                    "house"
+                ],
+                "creation_date": "20140304 21:23:42",
+                "last_update_date": "20140304 21:23:42",
+                "name": "realtor.com 1/31/2014",
+                "owner": "default",
+                "scores": {
+                    "ROUTE": {
+                        "percentage": 92.0,
+                        "raw": 33814.0
+                    }
+                }
+            }
+        },
+        {
+            "_id": "default|35.602801|-78.830704",
+            "type": "Feature",
+            "geometry": {
+                "coordinates": [
+                    -78.830704299999994,
+                    35.602800999999999
+                ],
+                "type": "Point"
+            },
+            "properties": {
+                "address": "Maggie Run Lane, Fuquay Varina, NC, USA",
+                "categories": [
+                    "house"
+                ],
+                "creation_date": "20140304 21:23:42",
+                "last_update_date": "20140304 21:23:42",
+                "name": "Maggie Run",
+                "owner": "default",
+                "scores": {
+                    "ROUTE": {
+                        "percentage": 16.0,
+                        "raw": 58662.0
+                    }
+                }
+            }
+        },
+        {
+            "_id": "default|35.674360|-78.594090",
+            "type": "Feature",
+            "geometry": {
+                "coordinates": [
+                    -78.594089999999994,
+                    35.67436
+                ],
+                "type": "Point"
+            },
+            "properties": {
+                "address": "317 Tallowwood Drive, Garner, NC 27529, USA",
+                "categories": [
+                    "house"
+                ],
+                "last_update_date": "20140304 21:23:43",
+                "creation_date": "20140304 21:23:43",
+                "name": "realtor.com 1/31/2014",
+                "owner": "default",
+                "scores": {
+                    "ROUTE": {
+                        "percentage": 72.0,
+                        "raw": 40219.0
+                    }
+                }
+            }
+        },
+        {
+            "_id": "default|35.674258|-78.638769",
+            "type": "Feature",
+            "geometry": {
+                "coordinates": [
+                    -78.638768999999996,
+                    35.674258000000002
+                ],
+                "type": "Point"
+            },
+            "properties": {
+                "last_update_date": "20140304 21:23:42",
+                "name": "North of Lake Benson",
+                "owner": "default",
+                "address": "2015 Gottwald Court, Garner, NC 27529, USA",
+                "categories": [
+                    "house"
+                ],
+                "creation_date": "20140304 21:23:42",
+                "scores": {
+                    "ROUTE": {
+                        "percentage": 95.0,
+                        "raw": 32841.0
+                    }
+                }
+
+            }
+        },
+        {
+            "_id": "default|35.548700|-78.576252",
+            "type": "Feature",
+            "geometry": {
+                "coordinates": [
+                    -78.576251999999997,
+                    35.548699900000003
+                ],
+                "type": "Point"
+            },
+            "properties": {
+                "last_update_date": "20140304 21:23:16",
+                "name": "Current Home",
+                "owner": "default",
+                "address": "80 Sophia Lane, Willow Spring, NC 27592, USA",
+                "categories": [
+                    "house"
+                ],
+                "creation_date": "20140304 21:23:16",
+                "scores": {
+                    "ROUTE": {
+                        "percentage": 0.0,
+                        "raw": 63660.0
+                    }
+                }
+            }
+        },
+        {
+            "_id": "default|35.674848|-78.667344",
+            "type": "Feature",
+            "geometry": {
+                "coordinates": [
+                    -78.667344,
+                    35.674847999999997
+                ],
+                "type": "Point"
+            },
+            "properties": {
+                "last_update_date": "20140304 21:23:42",
+                "name": "realtor.com 1/31/2014",
+                "owner": "default",
+                "address": "491 Waterville Street, Raleigh, NC 27603, USA",
+                "categories": [
+                    "house"
+                ],
+                "creation_date": "20140304 21:23:42",
+                "scores": {
+                    "ROUTE": {
+                        "percentage": 74.0,
+                        "raw": 39765.0
+                    }
+                }
+            }
+        },
+        {
+            "_id": "default|35.654199|-78.620186",
+            "type": "Feature",
+            "geometry": {
+                "coordinates": [
+                    -78.62018599999999,
+                    35.654198999999998
+                ],
+                "type": "Point"
+            },
+            "properties": {
+                "last_update_date": "20140304 21:23:43",
+                "name": "realtor.com 1/31/2014",
+                "owner": "default",
+                "address": "116 Honorable Place, Garner, NC 27529, USA",
+                "categories": [
+                    "house"
+                ],
+                "creation_date": "20140304 21:23:43",
+                "scores": {
+                    "ROUTE": {
+                        "percentage": 75.0,
+                        "raw": 39438.0
+                    }
+                }
+            }
+        },
+        {
+            "_id": "default|35.662709|-78.633836",
+            "type": "Feature",
+            "geometry": {
+                "coordinates": [
+                    -78.633836000000016,
+                    35.662709
+                ],
+                "type": "Point"
+            },
+            "properties": {
+                "address": "8004 Lake Shore Drive, Garner, NC 27529, USA",
+                "categories": [
+                    "house"
+                ],
+                "creation_date": "20140304 21:23:41",
+                "last_update_date": "20140304 21:23:41",
+                "name": "Lot near Lake Benson",
+                "owner": "default",
+                "scores": {
+                    "ROUTE": {
+                        "percentage": 48.0,
+                        "raw": 48299.0
+                    }
+                }
+            }
+        },
+        {
+            "_id": "default|35.703200|-78.685242",
+            "type": "Feature",
+            "geometry": {
+                "coordinates": [
+                    -78.685242000000002,
+                    35.703200000000002
+                ],
+                "type": "Point"
+            },
+            "properties": {
+                "last_update_date": "20140304 21:23:42",
+                "name": "realtor.com 1/31/2014",
+                "owner": "default",
+                "address": "1300 Cricklewood Court, Raleigh, NC 27603, USA",
+                "categories": [
+                    "house"
+                ],
+                "creation_date": "20140304 21:23:42",
+                "scores": {
+                    "ROUTE": {
+                        "percentage": 100.0,
+                        "raw": 31047.0
+                    }
+                }
+            }
+        },
+        {
+            "_id": "default|35.697703|-78.801252",
+            "type": "Feature",
+            "geometry": {
+                "coordinates": [
+                    -78.801251999999991,
+                    35.697702999999997
+                ],
+                "type": "Point"
+            },
+            "properties": {
+                "last_update_date": "20140304 21:23:43",
+                "name": "realtor.com 2014/02/28",
+                "owner": "default",
+                "address": "214 Shillings Chase Drive, Cary, NC 27518, USA",
+                "categories": [
+                    "house"
+                ],
+                "creation_date": "20140304 21:23:43",
+                "scores": {
+                    "ROUTE": {
+                        "percentage": 83.0,
+                        "raw": 36855.0
+                    }
+                }
+            }
+        },
+        {
+            "_id": "default|35.801530|-78.902486",
+            "type": "Feature",
+            "geometry": {
+                "coordinates": [
+                    -78.902486299999993,
+                    35.80153
+                ],
+                "type": "Point"
+            },
+            "properties": {
+                "last_update_date": "20140304 21:23:42",
+                "name": "Holland Farms Cary",
+                "owner": "default",
+                "address": "Holland Bend Drive, Cary, NC 27519, USA",
+                "categories": [
+                    "house"
+                ],
+                "creation_date": "20140304 21:23:42",
+                "scores": {
+                    "ROUTE": {
+                        "percentage": 51.0,
+                        "raw": 47222.0
+                    }
+                }
+            }
+        },
+        {
+            "_id": "default|35.644699|-78.647099",
+            "type": "Feature",
+            "geometry": {
+                "coordinates": [
+                    -78.647098999999997,
+                    35.644699000000003
+                ],
+                "type": "Point"
+            },
+            "properties": {
+                "last_update_date": "20140304 21:23:43",
+                "name": "realtor.com 2/8/2014",
+                "owner": "default",
+                "address": "4704 Swordfish Drive, Raleigh, NC 27603, USA",
+                "categories": [
+                    "house"
+                ],
+                "creation_date": "20140304 21:23:43",
+                "scores": {
+                    "ROUTE": {
+                        "percentage": 48.0,
+                        "raw": 48301.0
+                    }
+                }
+            }
+        },
+        {
+            "_id": "default|35.711861|-78.795169",
+            "type": "Feature",
+            "geometry": {
+                "coordinates": [
+                    -78.795169000000001,
+                    35.711860999999999
+                ],
+                "type": "Point"
+            },
+            "properties": {
+                "last_update_date": "20140304 21:23:43",
+                "name": "realtor.com 2014/02/28",
+                "owner": "default",
+                "address": "10832 Bexhill Drive, Cary, NC 27518, USA",
+                "categories": [
+                    "house"
+                ],
+                "creation_date": "20140304 21:23:43",
+                "scores": {
+                    "ROUTE": {
+                        "percentage": 85.0,
+                        "raw": 36238.0
+                    }
+                }
+            }
+        },
+        {
+            "_id": "default|35.670894|-78.674998",
+            "type": "Feature",
+            "geometry": {
+                "coordinates": [
+                    -78.674998000000002,
+                    35.670893900000003
+                ],
+                "type": "Point"
+            },
+            "properties": {
+                "last_update_date": "20140304 21:23:43",
+                "name": "realtor.com 1/31/2014",
+                "owner": "default",
+                "address": "121 Acentala Court, Raleigh, NC 27603, USA",
+                "categories": [
+                    "house"
+                ],
+                "creation_date": "20140304 21:23:43",
+                "scores": {
+                    "ROUTE": {
+                        "percentage": 65.0,
+                        "raw": 42782.0
+                    }
+                },
+                "view": 11
+            }
+        }
+    ]
+};
---
+++ src/main/webapp/data/trip.js
@@ -0,0 +1,137 @@
+var tripData = [
+    {
+        "_id": "default|Coffee Shop",
+        "categoryNames": [
+            "coffee"
+        ],
+        "creation_date": "20140316 21:52:03",
+        "frequency": 1.0,
+        "last_update_date": "20140316 21:52:03",
+        "name": "Coffee Shop",
+        "owner": "default"
+    },
+    {
+        "_id": "default|Work Crafts",
+        "categoryNames": [
+            "work",
+            "workshop"
+        ],
+        "creation_date": "20140316 21:52:03",
+        "frequency": 1.0,
+        "last_update_date": "20140316 21:52:03",
+        "name": "Work Crafts",
+        "owner": "default"
+    },
+    {
+        "_id": "default|Shopping",
+        "categoryNames": [
+            "shopping"
+        ],
+        "creation_date": "20140316 21:52:03",
+        "frequency": 2.0,
+        "last_update_date": "20140316 21:52:03",
+        "name": "Shopping",
+        "owner": "default"
+    },
+    {
+        "_id": "default|Work Normal",
+        "categoryNames": [
+            "work"
+        ],
+        "creation_date": "20140316 21:52:03",
+        "frequency": 2.0,
+        "last_update_date": "20140316 21:52:03",
+        "name": "Work Normal",
+        "owner": "default"
+    },
+    {
+        "_id": "default|Grocery",
+        "categoryNames": [
+            "grocery"
+        ],
+        "creation_date": "20140316 21:52:03",
+        "frequency": 2.0,
+        "last_update_date": "20140316 21:52:03",
+        "name": "Grocery",
+        "owner": "default"
+    },
+    {
+        "_id": "default|BJs",
+        "categoryNames": [
+            "warehouse"
+        ],
+        "creation_date": "20140316 21:52:03",
+        "frequency": 0.25,
+        "last_update_date": "20140316 21:52:03",
+        "name": "BJs",
+        "owner": "default"
+    },
+    {
+        "_id": "default|MOPS",
+        "categoryNames": [
+            "mops"
+        ],
+        "creation_date": "20140316 21:52:03",
+        "frequency": 0.5,
+        "last_update_date": "20140316 21:52:03",
+        "name": "MOPS",
+        "owner": "default"
+    },
+    {
+        "_id": "default|College",
+        "categoryNames": [
+            "college",
+            "daycare"
+        ],
+        "creation_date": "20140316 21:52:03",
+        "frequency": 2.0,
+        "last_update_date": "20140316 21:52:03",
+        "name": "College",
+        "owner": "default"
+    },
+    {
+        "_id": "default|Work Daycare",
+        "categoryNames": [
+            "daycare",
+            "work"
+        ],
+        "creation_date": "20140316 21:52:03",
+        "frequency": 2.0,
+        "last_update_date": "20140316 21:52:03",
+        "name": "Work Daycare",
+        "owner": "default"
+    },
+    {
+        "_id": "default|Park",
+        "categoryNames": [
+            "park"
+        ],
+        "creation_date": "20140316 21:52:03",
+        "frequency": 0.66000000000000003,
+        "last_update_date": "20140316 21:52:03",
+        "name": "Park",
+        "owner": "default"
+    },
+    {
+        "_id": "default|Chiropractor",
+        "categoryNames": [
+            "chiropractor"
+        ],
+        "creation_date": "20140316 21:52:03",
+        "frequency": 0.29999999999999999,
+        "last_update_date": "20140316 21:52:03",
+        "name": "Chiropractor",
+        "owner": "default"
+    },
+    {
+        "_id": "default|Museum",
+        "categoryNames": [
+            "museum"
+        ],
+        "creation_date": "20140316 21:52:03",
+        "frequency": 0.5,
+        "last_update_date": "20140316 21:52:03",
+        "name": "Museum",
+        "owner": "default"
+    }
+];
