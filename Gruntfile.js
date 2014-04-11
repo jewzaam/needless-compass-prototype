0788442954757456217529951e24763a9d6493d6
Sam Thomas <Samuel.Thomas2@fmr.com>
2014-04-11 17:07:13 -0400

    Copy working

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,12 +23,54 @@ module.exports = function(grunt) {
                     'dist/all.css': ['src/main/webapp/**/*.css']
                 }
             }
+        },
+
+        copy: {
+            modernizr: {
+                files: [{
+                    expand: true,
+                    flatten: true,
+                    src: ['src/main/webapp/js/modernizr.js'],
+                    dest: 'dist/'
+                }]
+            },
+            images: {
+                files: [{
+                    expand: true,
+                    flatten: true,
+                    // cwd: 'src/main/webapp/images/',
+                    src: ['src/main/webapp/images/**/*.png'],
+                    dest: 'dist/images'
+                }, {
+                    expand: true,
+                    flatten: true,
+                    // cwd: 'src/main/webapp/images/',
+                    src: ['src/main/webapp/images/**/*.ico'],
+                    dest: 'dist/'
+                }]
+            }
+        },
+
+        watch: {
+            options: {
+                livereload: true
+            },
+            css: {
+                files: 'src/main/webapp/css/**/*.css',
+                tasks: ['cssmin']
+            },
+            templates: {
+                files: 'src/main/webapp/templates/**/*.hbs',
+                tasks: ['assemble']
+            }
         }
     });
 
-
+    grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('assemble');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-    grunt.registerTask('default', ['assemble', 'cssmin']);
+    grunt.registerTask('default', ['assemble', 'cssmin', 'copy']);
 };
